40d8f021b24850585907143ff9a55d8a5f77fc15
Constance <constance.lesaffre@hotmail.fr>
2024-04-04 19:39:22 +0200

    update about

---
+++ app/javascript/controllers/show_skill_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="show-skill"
+export default class extends Controller {
+  static targets = ["description"]
+
+  connect() {
+    console.log("Hello, Stimulus!", this.element)
+  }
+
+  showDetails(event) {
+    event.preventDefault();
+
+    const card = event.currentTarget.closest(".skill-card");
+    const description = card.querySelector(".description");
+    // description.classList.toggle("hidden");
+
+    console.log(card, description);
+
+    card.classList.toggle("expanded");
+
+
+    if (card.classList.contains("expanded")) {
+      description.classList.remove("hidden");
+    } else {
+      description.classList.add("hidden");
+    }
+  }
+}
