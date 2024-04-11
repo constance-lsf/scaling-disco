e6305ed322ce0212ec4dff85e48cb81d044658e7
Constance <constance.lesaffre@hotmail.fr>
2024-04-11 15:57:13 +0200

    body font form contact

--- app/javascript/controllers/tab_category_controller.js
+++ app/javascript/controllers/tab_category_controller.js
@@ -8,33 +8,18 @@ export default class extends Controller {
     console.log("Tab Category Controller Connected")
   }
 
-  // changeTab(event) {
-  //   event.preventDefault()
-  //   console.log("changeTab called", event.currentTarget);
-
-  //   this.tabTargets.forEach(tab => {
-  //     console.log("tab", tab);
-  //     tab.classList.remove("active")
-  //   })
-  //   event.currentTarget.classList.add("active")
-  // }
-
   changeTab(event) {
     event.preventDefault()
     const currentTab = event.currentTarget
 
-    // Vérifier si l'élément cliqué est déjà actif
     if (currentTab.classList.contains('active')) {
-      // Supprimer la classe "active" sur l'élément cliqué
       currentTab.classList.remove('active')
     } else {
-      // Supprimer la classe "active" sur l'élément actif précédent
       const previousActiveTab = document.querySelector('.li-projects.active')
       if (previousActiveTab) {
         previousActiveTab.classList.remove('active')
       }
 
-      // Ajouter la classe "active" sur l'élément cliqué
       currentTab.classList.add('active')
     }
   }
